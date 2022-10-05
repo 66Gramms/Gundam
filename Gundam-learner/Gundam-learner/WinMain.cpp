5ca965a2dd4da02a6654b99335216e645c4778ed
66Gramms <meszaros@inf.u-szeged.hu>
2022-10-05 12:05:28 +0200

    Handle windows message loop

--- Gundam-learner/Gundam-learner/WinMain.cpp
+++ Gundam-learner/Gundam-learner/WinMain.cpp
@@ -1,5 +1,20 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE)
+			PostQuitMessage(0);
+		break;
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	// Register window class
@@ -7,7 +22,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -23,7 +38,24 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	long dwStyle = WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU;
 	HWND hWnd = CreateWindowEx(0, pClassName, L"Demo name", dwStyle, 200, 200, 640, 480, nullptr, nullptr, hInstance, nullptr);
 	ShowWindow(hWnd, SW_SHOW);
-	while (true);
+
+	// message pump
+	MSG msg;
+	BOOL gResult;
+	while (gResult = GetMessage(&msg, nullptr, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 
 	return 0;
 }
