4edfd114049c38a44dd6fcaf8dc5117e3695cb4d
66Gramms <meszarosricsi01@gmail.com>
2023-04-18 18:23:08 +0200

    Fix camera movement and fullscreen

--- Gundam-Solution/Gundam/App.cpp
+++ Gundam-Solution/Gundam/App.cpp
@@ -4,54 +4,83 @@
 #include "PyramidMaterial.h"
 #include "Camera.h"
 
-App::App() : window(800, 600, "Title") {}
+#if FULLSCREEN
+App::App() : window(GetSystemMetrics(SM_CXVIRTUALSCREEN), GetSystemMetrics(SM_CYVIRTUALSCREEN), "Gundam-engine") {}
+#else
+App::App() : window(800, 600, "Gundam-engine") {}
+#endif
 
 int App::Start()
 {
-	pDevice = window.GetGfx().GetPDevice().Get();
+    pDevice = window.GetGfx().GetPDevice().Get();
 
-	Camera* camera = new Camera();
-	camera->Initialize(window.GetWidth(), window.GetHeight(), &window);
-	this->camera = camera;
+    Camera* camera = new Camera();
+    camera->Initialize(window.GetWidth(), window.GetHeight(), &window);
+    this->camera = camera;
 
-	CubeMaterial* defaultMaterial = new CubeMaterial("shaders\\PixelShader.cso", "shaders\\VertexShader.cso", pDevice);
-	PyramidMaterial* pyramidMaterial = new PyramidMaterial("shaders\\PSDefault.cso", "shaders\\VSDefault.cso", pDevice);
+    CubeMaterial* defaultMaterial = new CubeMaterial("shaders\\PixelShader.cso", "shaders\\VertexShader.cso", pDevice);
+    PyramidMaterial* pyramidMaterial = new PyramidMaterial("shaders\\PSDefault.cso", "shaders\\VSDefault.cso", pDevice);
 
-	Model* cube = new Model();
-	cube->mesh = Model::CreateCube(pDevice);
-	cube->material = defaultMaterial;
-	cube->CreateConstantBuffer(pDevice);
-	models.push_back(cube);
+    Model* cube = new Model();
+    cube->mesh = Model::CreateCube(pDevice);
+    cube->material = defaultMaterial;
+    cube->CreateConstantBuffer(pDevice);
 
-	Model* pyramid = new Model();
-	pyramid->mesh = Model::CreatePyramid(pDevice);
-	pyramid->material = pyramidMaterial;
-	pyramid->CreateConstantBuffer(pDevice);
-	models.push_back(pyramid);
+    Model* pyramid = new Model();
+    pyramid->mesh = Model::CreatePyramid(pDevice);
+    pyramid->material = pyramidMaterial;
+    pyramid->CreateConstantBuffer(pDevice);
 
-	while (true)
-	{
-		if (const auto ecode = Window::ProcessMessages())
-		{
-			return *ecode;
-		}
-		
-		Update();
-	}
+    models.push_back(cube);
+    models.push_back(cube);
+    models.push_back(cube);
+    models.push_back(cube);
+
+    models.push_back(pyramid);
+    models.push_back(pyramid);
+    models.push_back(pyramid);
+    models.push_back(pyramid);
+
+    models.push_back(cube);
+    models.push_back(cube);
+    models.push_back(cube);
+    models.push_back(cube);
+
+    models.push_back(pyramid);
+    models.push_back(pyramid);
+    models.push_back(pyramid);
+    models.push_back(pyramid);
+
+    while (true)
+    {
+        if (const auto ecode = Window::ProcessMessages())
+        {
+            return *ecode;
+        }
+
+        Update();
+    }
 }
 
 void App::Update()
 {
-	if (window.keyboard.KeyIsPressed(VK_ESCAPE))
-	{
-		PostQuitMessage(0);
-	}
+    if (window.keyboard.KeyIsPressed(VK_ESCAPE))
+    {
+        PostQuitMessage(0);
+    }
 
-	const float c = sin(timer.Peek()) / 2.0f + 0.5f;
-	window.GetGfx().ClearBuffer(c, c, 1.0f);
-	camera->OnUpdate(timer.DeltaTime());
+    const float c = sin(timer.Peek()) / 2.0f + 0.5f;
+    window.GetGfx().ClearBuffer(c, c, 1.0f);
+    camera->OnUpdate(timer.DeltaTime());
+    int mod = 0;
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            window.GetGfx().DrawModel(models[mod], camera, ((i + j) % 2) ? timer.Peek() : -timer.Peek(), j * 5, i * 5);
+            mod += 1;
+        }
+    }
 
-	window.GetGfx().DrawModel(models[0], camera, timer.Peek(), sin(timer.Peek()) * 5, 0.0f);
-	window.GetGfx().DrawModel(models[1], camera, -timer.Peek(), 0.0f, cos(timer.Peek()) * 5);
-	window.GetGfx().EndFrame();
+    window.GetGfx().EndFrame();
 }
--- Gundam-Solution/Gundam/Camera.cpp
+++ Gundam-Solution/Gundam/Camera.cpp
@@ -4,101 +4,107 @@
 #include "Window.h"
 
 #include <complex>
+#define VK_KEY_W    0x57
+#define VK_KEY_A    0x41
+#define VK_KEY_S    0x53
+#define VK_KEY_D    0x44
 
 void Camera::Initialize(const uint32_t Width, const uint32_t Height, Window* window) noexcept
 {
-	using namespace DirectX;
+    using namespace DirectX;
 
-	this->window = window;
-	this->Width = Width;
-	this->Height = Height;
+    this->window = window;
+    this->Width = Width;
+    this->Height = Height;
 
-	Position = DirectX::XMVectorSet(0.0f, 5.0f, -10.0f, 0.0f);
-	Target = DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
-	Up = DirectX::XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
-	View = DirectX::XMMatrixLookAtLH(Position, Target, Up);
-	const auto AspectRatio = static_cast<float>(Width) / static_cast<float>(Height);
-	Projection = DirectX::XMMatrixPerspectiveFovLH(0.4f * 3.14f, AspectRatio, 0.1f, 1000.0f);
+    Position = DirectX::XMVectorSet(0.0f, 5.0f, -10.0f, 0.0f);
+    Target = DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+    Up = DirectX::XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+    View = DirectX::XMMatrixLookAtLH(Position, Target, Up);
+    const auto AspectRatio = static_cast<float>(Width) / static_cast<float>(Height);
+    Projection = DirectX::XMMatrixPerspectiveFovLH(0.4f * 3.14f, AspectRatio, 0.1f, 1000.0f);
 
-	OnUpdate(0.0f);
+    OnUpdate(0.0f);
 
-	RotationMatrix = DirectX::XMMatrixRotationRollPitchYaw(Pitch, Yaw, 0);
-	Target = DirectX::XMVector3TransformCoord(DefaultForward, RotationMatrix);
-	Target = DirectX::XMVector3Normalize(Target);
+    RotationMatrix = DirectX::XMMatrixRotationRollPitchYaw(Pitch, Yaw, 0);
+    Target = DirectX::XMVector3TransformCoord(DefaultForward, RotationMatrix);
+    Target = DirectX::XMVector3Normalize(Target);
 
-	Right = DirectX::XMVector3TransformCoord(DefaultRight, RotationMatrix);
-	Forward = DirectX::XMVector3TransformCoord(DefaultForward, RotationMatrix);
-	Up = DirectX::XMVector3Cross(Forward, Right);
+    Right = DirectX::XMVector3TransformCoord(DefaultRight, RotationMatrix);
+    Forward = DirectX::XMVector3TransformCoord(DefaultForward, RotationMatrix);
+    Up = DirectX::XMVector3Cross(Forward, Right);
 
-	Position += LeftRight * Right;
-	Position += BackForward * Forward;
+    Position += LeftRight * Right;
+    Position += BackForward * Forward;
 
-	LeftRight = 0.0f;
-	BackForward = 0.0f;
+    LeftRight = 0.0f;
+    BackForward = 0.0f;
 
-	Target = Position + Target;
+    Target = Position + Target;
 
-	View = DirectX::XMMatrixLookAtLH(Position, Target, Up);
+    View = DirectX::XMMatrixLookAtLH(Position, Target, Up);
 
 
 }
 
 void Camera::OnUpdate(const float deltaTime) noexcept
 {
-	using namespace DirectX;
+    using namespace DirectX;
 
-#ifdef _DEBUG
-	if (window->mouse.RightIsPressed())
-	{
-		Yaw += window->mouse.GetMouseDeltaX() * deltaTime;
-		Pitch += window->mouse.GetMouseDeltaY() * deltaTime;
-	}
+    if (window->mouse.RightIsPressed())
+    {
+        Yaw += window->mouse.GetMouseDeltaX() * deltaTime / 5.0f;
+        Pitch += window->mouse.GetMouseDeltaY() * deltaTime / 5.0f;
+    }
 
-	window->keyboard.EnableAutorepeat();
-	unsigned char pressedKey = window->keyboard.ReadKey().GetCode();
-	if (pressedKey == 'W')
-	{
-		BackForward += 15.0f * deltaTime * Speed;
-	}
-	if(pressedKey == 'S')
-	{
-		BackForward -= 15.0f * deltaTime * Speed;
-	}
-	if(pressedKey == 'A')
-	{
-		LeftRight -= 15.0f * deltaTime * Speed;
-	}
-	if(pressedKey == 'D')
-	{
-		LeftRight += 15.0f * deltaTime * Speed;
-	}
-#endif
-	View = DirectX::XMMatrixLookAtLH(Position, Target, Up);
-	RotationMatrix = DirectX::XMMatrixRotationRollPitchYaw(Pitch, Yaw, 0);
-	Target = DirectX::XMVector3TransformCoord(DefaultForward, RotationMatrix);
-	Target = DirectX::XMVector3Normalize(Target);
+    if (GetAsyncKeyState(VK_KEY_W) & 0x8000)
+    {
+        BackForward += 15.0f * deltaTime * Speed;
+    }
+    if (GetAsyncKeyState(VK_KEY_S) & 0x8000)
+    {
+        BackForward -= 15.0f * deltaTime * Speed;
+    }
+    if (GetAsyncKeyState(VK_KEY_A) & 0x8000)
+    {
+        LeftRight -= 15.0f * deltaTime * Speed;
+    }
+    if (GetAsyncKeyState(VK_KEY_D) & 0x8000)
+    {
+        LeftRight += 15.0f * deltaTime * Speed;
+    }
 
-	Right = DirectX::XMVector3TransformCoord(DefaultRight, RotationMatrix);
-	Forward = DirectX::XMVector3TransformCoord(DefaultForward, RotationMatrix);
-	Up = DirectX::XMVector3Cross(Forward, Right);
+    View = DirectX::XMMatrixLookAtLH(Position, Target, Up);
+    RotationMatrix = DirectX::XMMatrixRotationRollPitchYaw(Pitch, Yaw, 0);
+    Target = DirectX::XMVector3TransformCoord(DefaultForward, RotationMatrix);
+    Target = DirectX::XMVector3Normalize(Target);
 
-	Position += LeftRight * Right;
-	Position += BackForward * Forward;
+    Right = DirectX::XMVector3TransformCoord(DefaultRight, RotationMatrix);
+    Forward = DirectX::XMVector3TransformCoord(DefaultForward, RotationMatrix);
+    Up = DirectX::XMVector3Cross(Forward, Right);
 
-	LeftRight = 0.0f;
-	BackForward = 0.0f;
+    Position += LeftRight * Right;
+    Position += BackForward * Forward;
 
-	Target = Position + Target;
-	const auto AspectRatio = static_cast<float>(Width) / static_cast<float>(Height);
+    LeftRight = 0.0f;
+    BackForward = 0.0f;
+
+    Target = Position + Target;
+    const auto AspectRatio = static_cast<float>(Width) / static_cast<float>(Height);
 
 }
 
 DirectX::XMMATRIX Camera::GetViewMatrix() const noexcept
 {
-	return View;
+    return View;
 }
 
 DirectX::XMMATRIX Camera::GetProjectionMatrix() const noexcept
 {
-	return Projection;
+    return Projection;
 }
+
+#undef VK_KEY_W
+#undef VK_KEY_A
+#undef VK_KEY_S
+#undef VK_KEY_D
